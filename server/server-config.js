671847efd9613eeb08651e5eddc1a8ec3b7aa0a0
billywon <billy.y.won@gmail.com>
2017-03-15 11:00:11 -0700

    Implement React Router

---
+++ server/server-config.js
@@ -0,0 +1,10 @@
+let express = require('express');
+const database = require('./../database/index');
+
+const handler = require('./../lib/utility');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../client/dist`));
+
+module.exports = app;
